5dcfd1d0516f6cba8fc899338003bd426a038074
PabloDev09 <pmg11.cgl@gmail.com>
2024-01-15 12:51:38 +0100

    Version 13/01/2024

--- src/ejerciciosrecursividad/EjerciciosRecursividad.java
+++ src/ejerciciosrecursividad/EjerciciosRecursividad.java
@@ -3,14 +3,18 @@ package ejerciciosrecursividad;
 import java.util.Scanner;
 
 public class EjerciciosRecursividad {
-
-	public static int sumarDesdeNPrimerosNumeros(int n) {
+	
+	public static int sumarDesdeNPrimerosNumerosAux(int n, int y) {
 		if(n == 0) {
-			return 0;
+			return y;
 		}
 		else {
-			return n + sumarDesdeNPrimerosNumeros(n - 1);
+			return sumarDesdeNPrimerosNumerosAux(n - 1, n + y);
 		}
+	}
+	
+	public static int sumarDesdeNPrimerosNumeros(int n) {
+		return sumarDesdeNPrimerosNumerosAux(n, 0);
 	}
 	
 	public static String imprimirDesdeNaPrimerosNumeros(int n) {
@@ -47,6 +51,53 @@ public class EjerciciosRecursividad {
 			return n%10 + "" + imprimirInvertido(n/10);
 		}
 	}
+	
+	private static int reverseNumberAux(int n, int result) {
+		int digit = 0;
+		if(n == 0) {
+			return result;
+		}else {
+			digit = n%10;
+			result = digit + result*10;
+			n = n/10;
+			
+			return reverseNumberAux(n, result);
+		}
+	}
+	
+	public static int reverseNumber(int n) {
+		return reverseNumberAux(n, 0);
+	}
+	
+	public static int terminoFibonacci(int n) {
+		if(n <= 1) {
+			return 1;
+		}
+		else {
+			return terminoFibonacci(n - 1) + terminoFibonacci(n - 2);
+		}
+	}
+	
+	public static int maximoComunDivisor(int n, int x) {
+		if(x == 0) {
+			return n;
+		}else {
+			return maximoComunDivisor(x, n%x);
+		}
+	}
+	
+	public static int maximoComunDivisorMejorada(int n, int x) {
+		if(n <= x) {
+			return maximoComunDivisor(x, n);
+		}else {
+			return maximoComunDivisor(x, n%x);
+		}
+	}
+	
+	
+	public static int minimoComunMultiplo(int n, int x) {
+		return (n * x) / maximoComunDivisor(n, x);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
@@ -59,6 +110,9 @@ public class EjerciciosRecursividad {
 		System.out.println("La suma desde 1 hasta " + n + " es: " + imprimirDesdePrimerosNumerosaN(n));
 		System.out.println("La suma desde 1 hasta " + n + " es: " + cantidadDigitos(n));
 		System.out.println(imprimirInvertido(n));
+		System.out.println(terminoFibonacci(6));
+		System.out.println(maximoComunDivisorMejorada(9,9));
+		System.out.println(minimoComunMultiplo(20, 16));
 	}
 
 }
